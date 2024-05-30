cce2d2098cfefeb009118598267d78cee20c955a
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-05-30 13:12:12 -0400

    refactored some magic numbers in o_ground.c

--- o_ground.c
+++ o_ground.c
@@ -1,5 +1,6 @@
 #include "o_ground.h"
 #include "constants.h"
+#include "colors.h"
 
 static const SDL_Rect rect = {
     .x = 0,
@@ -9,6 +10,10 @@ static const SDL_Rect rect = {
 };
 
 void ground_draw (ctx_t * ctx) {
-    SDL_SetRenderDrawColor(ctx->renderer, 11, 1, 26, 0);
+    static rgba_t ground = { .r = 11, .g = 1, .b = 26, .a = 0 };
+    SDL_SetRenderDrawColor(ctx->renderer, ground.r,
+                                          ground.g,
+                                          ground.b,
+                                          ground.a);
     SDL_RenderFillRect(ctx->renderer, &rect);
 }
