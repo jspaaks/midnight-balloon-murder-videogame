4e3f6d56e14e007619959231cb90d012f5c8c8b2
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-05-31 17:27:56 -0400

    updated legend with colored low-ammo indicator

--- o_legend.c
+++ o_legend.c
@@ -4,7 +4,36 @@
 #include "o_balloons.h"
 #include "colors.h"
 
-void o_legend_draw (ctx_t * ctx) {
+static void o_legend_bars_draw (ctx_t *);
+static void o_legend_nbullets_warning_draw (ctx_t *);
+
+static void o_legend_nbullets_warning_draw (ctx_t * ctx) {
+    static const rgba_t magenta = { .r = 200, .g =   0, .b = 255, .a = 0 };
+    static const rgba_t red     = { .r = 200, .g =   0, .b =   0, .a = 0 };
+    static const rgba_t orange  = { .r = 200, .g = 161, .b =   0, .a = 0 };
+    static const rgba_t green   = { .r =   0, .g = 161, .b =   0, .a = 0 };
+    static const rgba_t bgcolor = { .r =   0, .g =  22, .b =  43, .a = 0 };
+    if (ctx->nbullets < 5) {
+        SDL_SetRenderDrawColor(ctx->renderer, magenta.r, magenta.g, magenta.b, magenta.a);
+    } else if (ctx->nbullets < 10) {
+        SDL_SetRenderDrawColor(ctx->renderer, red.r, red.g, red.b, red.a);
+    } else if (ctx->nbullets < 20) {
+        SDL_SetRenderDrawColor(ctx->renderer, orange.r, orange.g, orange.b, orange.a);
+    } else if (ctx->nbullets < 30) {
+        SDL_SetRenderDrawColor(ctx->renderer, green.r, green.g, green.b, green.a);
+    } else {
+        SDL_SetRenderDrawColor(ctx->renderer, bgcolor.r, bgcolor.g, bgcolor.b, bgcolor.a);
+    }
+    SDL_Rect rect = {
+        .x = 80,
+        .y = 80,
+        .w = 195,
+        .h = 38,
+    };
+    SDL_RenderFillRect(ctx->renderer, &rect);
+}
+
+static void o_legend_bars_draw (ctx_t * ctx) {
     static const rgba_t hit    = { .r =   0, .g = 161, .b =   0, .a = 0 };
     static const rgba_t middle = { .r = 128, .g = 128, .b = 128, .a = 0 };
     static const rgba_t miss   = { .r = 200, .g =   0, .b =   0, .a = 0 };
@@ -19,7 +48,7 @@ void o_legend_draw (ctx_t * ctx) {
         .x = 60,
         .y = 50,
         .w = 15,
-        .h = 20,
+        .h = 18,
     };
 
     SDL_SetRenderDrawColor(ctx->renderer, hit.r, hit.g, hit.b, hit.a);
@@ -37,6 +66,11 @@ void o_legend_draw (ctx_t * ctx) {
         rect.x += (rect.w + 5);
         SDL_RenderFillRect(ctx->renderer, &rect);
     }
+}
+
+void o_legend_draw (ctx_t * ctx) {
+    o_legend_bars_draw(ctx);
+    o_legend_nbullets_warning_draw(ctx);
 }
 
 ctx_t * o_legend_update (ctx_t * ctx) {
