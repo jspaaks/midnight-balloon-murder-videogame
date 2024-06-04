9f8ffe1863c965c6f2155a4b91f2447c02e0c6b9
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-06-04 14:11:29 -0400

    formatting

--- colors.c
+++ colors.c
@@ -4,15 +4,15 @@
 
 ctx_t * colors_init (ctx_t * ctx) {
     ctx->colors = (colors_t) {
-        .bg      = (SDL_Color) { .r =   0, .g =  22, .b =  43, .a = 0 },
-        .green   = (SDL_Color) { .r =   0, .g = 161, .b =   0, .a = 0 },
-        .hit     = (SDL_Color) { .r =   0, .g = 161, .b =   0, .a = 0 },
-        .magenta = (SDL_Color) { .r = 200, .g =   0, .b = 255, .a = 0 },
-        .middle  = (SDL_Color) { .r = 128, .g = 128, .b = 128, .a = 0 },
-        .miss    = (SDL_Color) { .r = 200, .g =   0, .b =   0, .a = 0 },
-        .orange  = (SDL_Color) { .r = 200, .g = 161, .b =   0, .a = 0 },
-        .red     = (SDL_Color) { .r = 200, .g =   0, .b =   0, .a = 0 },
-        .white   = (SDL_Color) { .r = 255, .g = 255, .b = 255, .a = 0 },
+        .bg      = (SDL_Color) { .r =   0, .g =  22, .b =  43, .a =   0 },
+        .green   = (SDL_Color) { .r =   0, .g = 161, .b =   0, .a =   0 },
+        .hit     = (SDL_Color) { .r =   0, .g = 161, .b =   0, .a =   0 },
+        .magenta = (SDL_Color) { .r = 200, .g =   0, .b = 255, .a =   0 },
+        .middle  = (SDL_Color) { .r = 128, .g = 128, .b = 128, .a =   0 },
+        .miss    = (SDL_Color) { .r = 200, .g =   0, .b =   0, .a =   0 },
+        .orange  = (SDL_Color) { .r = 200, .g = 161, .b =   0, .a =   0 },
+        .red     = (SDL_Color) { .r = 200, .g =   0, .b =   0, .a =   0 },
+        .white   = (SDL_Color) { .r = 255, .g = 255, .b = 255, .a =   0 },
     };
     return ctx;
 }
