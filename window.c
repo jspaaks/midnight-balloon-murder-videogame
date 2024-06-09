b0d02a1328021eb3004d5268da62db204c0d6b8c
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-06-09 11:59:36 -0400

    added an assert to window initialization

--- window.c
+++ window.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include "SDL_error.h"
 #include "SDL_log.h"
@@ -12,6 +13,7 @@ ctx_t * window_deinit(ctx_t * ctx) {
 }
 
 ctx_t * window_init(ctx_t * ctx) {
+    assert(ctx->scene.tgt.w != 0 && "scene needs to be initialized before window");
     int flags = SDL_WINDOW_RESIZABLE;
     ctx->window = SDL_CreateWindow("Midnight Balloon Murder", SDL_WINDOWPOS_CENTERED,
                                    SDL_WINDOWPOS_CENTERED, ctx->scene.tgt.w, ctx->scene.tgt.h, flags);
