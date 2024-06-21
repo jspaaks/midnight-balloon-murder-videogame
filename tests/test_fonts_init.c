2a7dff829392dd46fc5347930c11584dc9729d8d
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-06-21 14:28:39 -0400

    added tests to new subdirecotry tests; added tests/CMakeLists.txt

---
+++ tests/test_fonts_init.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include "deinit.h"
+#include "fonts.h"
+#include "types.h"
+
+int main() {
+    fonts_t fonts;
+    deinit_prepare_fonts(&fonts);
+
+    fonts = fonts_init();
+    bool cond = fonts.regular != NULL;
+    return cond ? 0 : 1;
+}
