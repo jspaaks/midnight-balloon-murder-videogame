344f80db1f1e5f7a7ec4f0afe207aec7ee7ec0ae
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-06-23 15:13:17 -0400

    loading assets now relative to path of executable

--- tests/test_fonts_init.c
+++ tests/test_fonts_init.c
@@ -7,7 +7,8 @@ int main(void) {
     fonts_t fonts;
     deinit_prepare_fonts(&fonts);
 
-    fonts = fonts_init();
+    char * basepath = SDL_GetBasePath();
+    fonts = fonts_init(basepath);
     bool cond = fonts.regular != NULL;
     return cond ? 0 : 1;
 }
